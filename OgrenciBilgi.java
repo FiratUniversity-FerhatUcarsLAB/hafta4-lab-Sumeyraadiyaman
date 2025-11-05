f6011ac7d0435b330ba76a1242ec32fabdbb09c8
Sümeyra Adıyaman <250541012@firat.edu.tr>
2025-11-05 10:23:32 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -12,49 +12,37 @@
 import java.util.Scanner;
 
 public class OgrenciBilgi {
-    public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+    public static void main(String[] args) { 
+        // değişkenler alınıyor
+        Scanner input = new Scanner(System.in);
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+
+        System.out.print("Adınızı Giriniz: ");
+        String ad = input.nextLine();
+
+        System.out.print("Soyadınız Giriniz: ");
+        String soyad = input.nextLine();
+
+        System.out.print("Öğrenci Numaranız: ");
+        int ogrenci_no = input.nextInt();
+
+        System.out.print("Yaşınız Giriniz: ");
+        int yas = input.nextInt();
+
+        System.out.print("GPA (0,00-4,00): ");
+        float gpa = input.nextFloat();
+
+        //bilgiler ekrana bastırılıyor
+
+        System.out.println("=====ÖĞRENCİ BİLGİ SİSTEMİ====");
+        System.out.printf("AD-SOYAD : %s %s ", ad, soyad);
+        System.out.printf("\nÖğrenci No : %d", ogrenci_no);
+        System.out.printf("\nYaş : %d", yas);
+        System.out.printf("\nGPA : %.2f", gpa);
     }
 }
+
+
+
+
+
